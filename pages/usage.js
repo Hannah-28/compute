7548c578002436160a262ed3451419b43cc56621
Hannah-28 <omomurewale@gmail.com>
2024-04-01 17:51:03 +0100

    Usage Page Changes

--- pages/usage.js
+++ pages/usage.js
@@ -11,6 +11,7 @@ export default function Usage() {
   const dispatch = useDispatch();
   const getUsageState = useSelector((s) => s.getUsage);
   const [usage, setUsage] = useState([]);
+  const [filtered, setFiltered] = useState([0]);
 
   const id = localStorage.getItem('projectID');
 
@@ -63,13 +64,25 @@ export default function Usage() {
       style: {
         backgroundColor: '#05052D',
         color: '#FFF',
+        fontWeight: '700',
+        textTransform: 'uppercase',
       },
     },
   };
 
+  const handleFilter = (event) => {
+    const newData = usage.data.filter((row) =>
+      moment(row.start_date).format('MM/DD/YYYY').includes(event.target.value)
+   // moment(row.start_date).format('MM/DD/YYYY').includes(moment(event.target.value).format("MM/DD/YYYY"))
+      );
+     
+      console.log(moment(event.target.value).format("MM/DD/YYYY"));
+    setFiltered(newData);
+  };
+  // console.log(filtered.length, 'Filtered', usage?.data?.length, 'Original');
   return (
     <UserSidebar title="Usage">
-      <div className="h-screen py-5 px-3 my-auto">
+      <div className="min-h-screen py-5 px-3 my-auto">
         {usage?.data?.length === undefined ? (
           <div
             className="animate-spin inline-block size-8 border-[3px] border-current border-t-transparent text-primary-dark rounded-full dark:text-primary-dark"
@@ -77,20 +90,34 @@ export default function Usage() {
             aria-label="loading"
           ></div>
         ) : (
-          <>
-            <h1 className="mb-8 text-2xl font-bold">Usage</h1>
-            <div className='w-full'>
-              <DataTable
-                columns={columns}
-                customStyles={customStyles}
-                data={usage.data}
-                pagination
-                // className='w-full 2xl:max-w-screen-lg px-5 pt-5 bg-primary-dark'
-                className='w-full 2xl:max-w-screen-lg'
+        <>
+          <h1 className="mb-8 text-2xl font-bold">Usage</h1>
+          <div className="w-full">
+            <div style={{ display: 'flex', justifyContent: 'right' }}>
+              <input
+                type="text"
+                placeholder="Search start date..."
+                onChange={handleFilter}
+                style={{ width: '10em', marginBottom: '1em' }}
               />
+               {/* <input
+                type="date"
+                placeholder="Search..."
+                onChange={handleFilter}
+                style={{ width: '10em', marginBottom: '1em' }}
+              /> */}
             </div>
-          </>
-        )}
+            <DataTable
+              columns={columns}
+              customStyles={customStyles}
+              // changes to be made to the filtered.length later because of initializing filtered
+              data={filtered.length === 1 ? usage.data : filtered}
+              pagination
+              className="w-full 2xl:max-w-screen-lg"
+            />
+          </div>
+        </>
+       )}
       </div>
     </UserSidebar>
   );
